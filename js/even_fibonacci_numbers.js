603475a4806d5dad7956ff90be1b8ed378a5022d
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-24 12:24:07 -0200

    fix - order

--- js/even_fibonacci_numbers.js
+++ js/even_fibonacci_numbers.js
@@ -1,7 +1,7 @@
 function fibonacci(){
 
-  var a = 1, b = 2, c = 0, somatotal = 2;
-  var valor = 0;
+  var valor = 0, a = 1, b = 2, c = 0, somatotal = 2;
+  var
 
   valor = document.getElementById("number").value;
 
@@ -10,7 +10,6 @@ function fibonacci(){
       c = a + b;
       a = b;
       b = c;
-
         if(c % 2 == 0)
         {
           somatotal += c;
